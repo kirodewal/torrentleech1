21497b75402d74c9c1ca2910b59c3926e02bf872
Kamlesh Kirodewal <kkirodewal@gmail.com>
2020-11-05 11:06:27 +0530

    Add files via upload

---
+++ tobrot/config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1204748326:AAE83nb8j7WNyHZ6rUZqt2Qff_hEbA2eooY"
+  APP_ID = 924859
+  API_HASH = "a4c9a18cf4d8cb24062ff6916597f832"
+  AUTH_CHANNEL = -1001199115006
